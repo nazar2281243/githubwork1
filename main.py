a5ca0349b67d895e6cc1d647dc2a4b77550694d5
student1 <ushanov.sanya121@mail.ru>
2022-10-28 10:18:39 +0300

    commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hellow World")
+class Mytry:
+
+    def __init__(self, a, aname):
+        self.name = aname
+        self.a = a
+
+    def print_obj(self):
+        print(self.name, self.a)
+
+obj = Mytry(aname="lol", a=3)
+Mytry.print_obj(object)
